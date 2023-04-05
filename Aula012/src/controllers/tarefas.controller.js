edf792473fa649e61ee103f827f260f91ad6985f
GustavoDuartte <gustavoduartejag@gmail.com>
2023-04-05 11:29:15 -0300

    rec

--- Aula012/src/controllers/tarefas.controller.js
+++ Aula012/src/controllers/tarefas.controller.js
@@ -4,13 +4,39 @@ const Tarefa = require("../models/tarefas");
 const app = (req, res) => {
   con.query(new Tarefa(req.body).read(), (err, result) => {
     if (err) {
-      res.render("erro", { err: err });
+      res.send("Erro: " + err).end();
     }
 
-    res.render("index", { titulo: "tarefas a fazer", dados: result });
+    res.render("index", { titulo: "Tarefas a Fazer", dados: result });
+  });
+};
+
+const create = (req, res) => {
+  con.query(new Tarefa(req.body).create(), (err, result) => {
+    if (err) {
+      res.send("Erro: " + err).end();
+    }
+    res.status(201).redirect("/");
+  });
+};
+
+const update = (req, res) => {
+  con.query(new Tarefa(req.body).update(), (err, result) => {
+    if (result.affectedRows > 0) res.redirect("/");
+    else res.send("Erro: " + err).end();
+  });
+};
+
+const excluir = (req, res) => {
+  con.query(new Tarefa(req.params).del(), (err, result) => {
+    if (result.affectedRows > 0) res.redirect("/");
+    else res.send("Erro: " + err).end();
   });
 };
 
 module.exports = {
   app,
+  excluir,
+  create,
+  update,
 };
--- Aula012/src/routes/tarefas.routes.js
+++ Aula012/src/routes/tarefas.routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const Tarefacontroller = require("../controllers/tarefas.controller");
 
+router.post("/create", Tarefacontroller.create);
 router.get("/", Tarefacontroller.app);
+router.put("/update", Tarefacontroller.update);
+router.delete("/del/:id", Tarefacontroller.excluir);
 
 module.exports = router;
